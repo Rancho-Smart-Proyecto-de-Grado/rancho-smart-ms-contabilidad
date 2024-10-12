c9a85dcd2b1a61846beb0c7b8870e6fd2c44eb85
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 03:08:49 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ ms-contabilidad/src/main/java/com/rancho_smart/ms_contabilidad/MsContabilidadApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_contabilidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsContabilidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsContabilidadApplication.class, args);
+	}
+
+}
---
+++ ms-contabilidad/src/test/java/com/rancho_smart/ms_contabilidad/MsContabilidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_contabilidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsContabilidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
